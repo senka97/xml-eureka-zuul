a4267d0fa77b38f9552415b1c990b46d915f868a
Senka <senka.soic@uns.ac.rs>
2020-05-16 13:55:13 +0200

    Example for rbac in spring security added.

--- zuul/src/main/java/com/team19/zuul/client/UserClient.java
+++ zuul/src/main/java/com/team19/zuul/client/UserClient.java
@@ -2,10 +2,26 @@ package com.team19.zuul.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "user")
 public interface UserClient {
 
-    @GetMapping("/hello")
-    String hello();
+    @GetMapping("/helloAdmin")
+    String helloAdmin();
+
+    @GetMapping("/helloClient")
+    String helloClient();
+
+    @GetMapping("/helloAgent")
+    String helloAgent();
+
+    @PostMapping("/helloAdmin")
+    String postAdmin();
+
+    @PostMapping("/helloAgent")
+    String postAgent();
+
+    @PostMapping("/helloClient")
+    String postClient();
 }
