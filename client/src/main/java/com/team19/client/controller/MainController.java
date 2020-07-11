28dac667684fccb09b448e3fea82bdb115af19c5
Senka <senka.soic@uns.ac.rs>
2020-07-11 22:13:19 +0200

    Docker-compose edited.

--- client/src/main/java/com/team19/client/controller/MainController.java
+++ client/src/main/java/com/team19/client/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-        @RequestMapping({ "", "/login", "/registration" ,"/home", "/homePage", "/cart", "/vehicle/**", "/post/**", "/adminProfile", "/notAuthorized", "/activate-account/**", "/newRequests", "/requests/**", "/reservations", "/myRequests", "/priceLists", "/statistic" })
+        @RequestMapping({ "", "/login", "/registration" ,"/home", "/homePage", "/cart", "/vehicle/**", "/post/**", "/adminProfile", "/notAuthorized", "/activate-account/**", "/newRequests", "/requests/**", "/reservations", "/myRequests", "/priceLists", "/statistic", "/change-password", "/reset-password", "/forgot-password" })
         public String gui() {
             return "forward:/index.html";
         }
